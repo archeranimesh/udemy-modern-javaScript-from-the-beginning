fc822ac59dc1023843fa9bb1bdac745fba7be5f1
Animesh Bhadra <animeshb@archerimagine.com>
2022-10-13 21:53:46 +0530

    📦 NEW: String and String Methods.

--- SRC/Section-02-Javascript-Language-Fundamentals/app.js
+++ SRC/Section-02-Javascript-Language-Fundamentals/app.js
@@ -1,31 +1,67 @@
-const val1 = 100;
-const val2 = 60;
-let val;
+const firstName = "John";
+const lastName = "Doe";
+const age = 30;
+const str = "hello my name is John Doe."
 
-// Simple Math with Numbers
-val = val1 + val2;      // Addition
-val = val1 - val2;      // Substraction
-val = val1 * val2;      // Multiplication
-val = val1 / val2;      // Division
-val = val1 % val2       // Remainder
+val = firstName + lastName; // JohnDoe
 
-// Math Object
-val = Math.PI;
+// Concatenation
+val = firstName + " " +lastName;    //John Doe
 
-// Math function.
-val = Math.round(2.4);      // 2
-val = Math.round(2.8);      // 3
-val = Math.ceil(2.4);       // 3
-val = Math.floor(2.8);      // 2
-val = Math.sqrt(64);        // 8
-val = Math.abs(-3);         // 3
-val = Math.pow(8,2);        // 64
-val = Math.min(1,2,3,4);    // 1
-val = Math.max(1,2,3,4);    // 4
-val = Math.random();        // number between 0 and 1
-val = Math.floor((Math.random() * 20) + 1 );      // random no between 1-20
+// Append
+val = "John ";
+val += "Doe"
 
+// Sentence
+val = "Hello, My name is " +firstName + " and I am " +age +" old.";     //Hello, My name is John and I am 30 old.
 
-// output
+// Escaping
+val = 'That\'s awesome, I can\'t wait';     // That's awesome, I can't wait
+
+// String Property
+// length
+val = firstName.length;     // 4
+
+// concat
+val = firstName.concat(" ", lastName);
+
+// change case.
+val = firstName.toUpperCase();  // JOHN
+val = firstName.toLowerCase();  // john
+
+// characters from string
+val = firstName[0];     // J
+
+// indexOf
+val = firstName.indexOf("n");   // 3, -1 if not found.
+
+val = "William"
+val = val.indexOf("l");     // 2
+val = "William"
+val = val.lastIndexOf("l"); // 3, if there are two char, it will return last one.
+
+// charAt
+val = firstName.charAt(2); // h
+
+// Last Character of the string
+val = firstName.charAt(firstName.length - 1);
+val = firstName.slice(-1);
+
+// Substring
+val = firstName.substring(0,2); // Jo, 2 is not included.
+
+// Slice
+val = firstName.slice(0,2);     // Jo
+// last 3 digit
+val = firstName.slice(-3);
+
+// Split
+val = str.split(" ");       // [ "hello", "my", "name", "is", "John", "Doe." ]
+
+// replace
+val = str.replace("John", "Jane");
+
+// includes
+val = str.includes("hello");    // true
+
 console.log(val);
-console.log(typeof val);
---
+++ SRC/Section-02-Javascript-Language-Fundamentals/app_05.js
@@ -0,0 +1,31 @@
+const val1 = 100;
+const val2 = 60;
+let val;
+
+// Simple Math with Numbers
+val = val1 + val2;      // Addition
+val = val1 - val2;      // Substraction
+val = val1 * val2;      // Multiplication
+val = val1 / val2;      // Division
+val = val1 % val2       // Remainder
+
+// Math Object
+val = Math.PI;
+
+// Math function.
+val = Math.round(2.4);      // 2
+val = Math.round(2.8);      // 3
+val = Math.ceil(2.4);       // 3
+val = Math.floor(2.8);      // 2
+val = Math.sqrt(64);        // 8
+val = Math.abs(-3);         // 3
+val = Math.pow(8,2);        // 64
+val = Math.min(1,2,3,4);    // 1
+val = Math.max(1,2,3,4);    // 4
+val = Math.random();        // number between 0 and 1
+val = Math.floor((Math.random() * 20) + 1 );      // random no between 1-20
+
+
+// output
+console.log(val);
+console.log(typeof val);
